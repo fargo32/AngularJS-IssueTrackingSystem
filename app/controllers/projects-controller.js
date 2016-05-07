c0654a8523acebd0dafdf7983cd312526d27d5a9
fargo32 <imkitukov@gmail.com>
2016-05-07 15:30:17 +0300

    more projects functionality and views

--- app/controllers/projects-controller.js
+++ app/controllers/projects-controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('issueTracker.controllers.projects', [])
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when('/projects', {
                 templateUrl: 'views/projects/all-projects.html',
@@ -24,15 +24,23 @@ angular.module('issueTracker.controllers.projects', [])
                     requiresAdmin: true
                 }
             })
+            .when('/projects/:id', {
+                templateUrl: 'views/projects/project.html',
+                controller: 'ViewProjectController',
+                access: {
+                    requiresAdmin: true
+                }
+            })
 
     }])
+
     .controller('ProjectsController', [
         '$scope',
         '$location',
         'projectsService',
         'notificationService',
         'PAGE_SIZE',
-        function($scope, $location, projects, notificationService, pageSize) {
+        function ($scope, $location, projectsService, notificationService, pageSize) {
             $scope.projectsParams = {
                 pageSize: pageSize,
                 pageNumber: 1
@@ -40,7 +48,7 @@ angular.module('issueTracker.controllers.projects', [])
 
             $scope.allUsers();
 
-            $scope.addProject = function(project) {
+            $scope.addProject = function (project) {
                 projectsService.addProject(project)
                     .then(function success(data) {
                         $location.path('projects/' + data.Id);
@@ -49,7 +57,7 @@ angular.module('issueTracker.controllers.projects', [])
                     });
             };
 
-            $scope.getAllProjects = function() {
+            $scope.getAllProjects = function () {
                 projectsService.getAllProjects($scope.projectsParams)
                     .then(function success(data) {
                         $scope.allProjects = data.Projects;
@@ -67,13 +75,13 @@ angular.module('issueTracker.controllers.projects', [])
         'projectsService',
         'PAGE_SIZE',
         'notificationService',
-        function($scope, projectsService, pageSize, notificationService) {
+        function ($scope, projectsService, pageSize, notificationService) {
             $scope.myProjectsParams = {
                 pageSize: pageSize,
                 pageNumber: 1
             };
 
-            $scope.getMyProjects = function() {
+            $scope.getUserProjects = function () {
                 projectsService.getUserProjects($scope.myProjectsParams)
                     .then(function success(data) {
                         $scope.myProjects = data.Projects;
@@ -83,5 +91,41 @@ angular.module('issueTracker.controllers.projects', [])
                     });
             };
 
-            $scope.getMyProjects();
+            $scope.getUserProjects();
+        }])
+
+    .controller('ViewProjectController', [
+        '$scope',
+        '$routeParams',
+        'projectsService',
+        'notificationService',
+        function ($scope, $routeParams, projectsService, notificationService) {
+
+            projectsService.getProjectById($routeParams.id)
+                .then(function success(data) {
+                    $scope.currentProject = data;
+    console.log(data);
+                    if (data.Lead.Id === JSON.parse(sessionStorage['currentUser']).Id) {
+                        $scope.isLeadOfProject = true;
+                    } else {
+                        $scope.isLeadOfProject = false;
+                    }
+    console.log($scope.isLeadOfProject);
+                    $scope.currentProjectLabels = [];
+                    $scope.currentProjectPriorities = [];
+
+                    data.Labels.forEach(function (l) {
+                        $scope.currentProjectLabels.push(l.Name);
+                    });
+                    console.log($scope.currentProjectLabels);
+                    console.log($scope.currentProject);
+
+                    data.Priorities.forEach(function (p) {
+                        $scope.currentProjectPriorities.push(p.Name);
+                    });
+                }, function error(err) {
+                    notificationService.showError('Unable to get project', err);
+                });
+
+
         }]);
