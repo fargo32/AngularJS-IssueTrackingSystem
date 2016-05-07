6a2c9bf1e6eeae2ec7730cd8f88af93a4c7b1877
fargo32 <imkitukov@gmail.com>
2016-05-07 23:57:40 +0300

    edit issues done, and other fixes

--- app/controllers/issues-controller.js
+++ app/controllers/issues-controller.js
@@ -4,8 +4,15 @@ angular.module('issueTracker.controllers.issues', [])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when('/issues/:id', {
-                templateUrl: 'views/partials/issues/issue.html',
+                templateUrl: 'views/issues/issue.html',
                 controller: 'ViewIssueController',
+                access: {
+                    requiresLogin: true
+                }
+            })
+            .when('/issues/:id/edit', {
+                templateUrl: 'views/issues/edit-issue.html',
+                controller: 'EditIssueController',
                 access: {
                     requiresLogin: true
                 }
@@ -15,13 +22,11 @@ angular.module('issueTracker.controllers.issues', [])
         '$scope',
         '$routeParams',
         'issuesService',
-        'comments',
         'projectsService',
         'notificationService',
-        function ($scope, $routeParams, issuesService, comments, projectsService, notificationService) {
-            $scope.issueComment = {};
+        function ($scope, $routeParams, issuesService, projectsService, notificationService) {
 
-            $scope.getIssueById = function () {
+            $scope.getIssueById = function() {
                 issuesService.getIssueById($routeParams.id)
                     .then(function success(data) {
                         $scope.currentIssue = data;
@@ -56,6 +61,60 @@ angular.module('issueTracker.controllers.issues', [])
 
 
             //todo EditController
+
+            $scope.getIssueById();
         }])
 
+    .controller('EditIssueController', [
+        '$scope',
+        '$routeParams',
+        '$location',
+        'issuesService',
+        'projectsService',
+        'notificationService',
+        function ($scope, $routeParams, $location, issuesService, projectsService, notificationService) {
+
+            $scope.allUsers();
+
+            issuesService.getIssueById($routeParams.id)
+                .then(function success(data) {
+                    $scope.currentIssue = data;
+                    $scope.currentIssueDueDateLocal = new Date(data.DueDate);
+                    $scope.issuePriority = data.Priority.Id;
+                    $scope.currentIssueLabels = [];
+
+                    data.Labels.forEach(function (label) {
+                        $scope.currentIssueLabels.push(label.Name);
+                    });
+
+                    projectsService.getProjectById(data.Project.Id)
+                        .then(function success(data) {
+                            $scope.projectPriorities = data.Priorities;
+                        });
+                }, function error(err) {
+                    notificationService.showError('Unable to get issue', err);
+                });
+
+            $scope.editIssue = function () {
+                if (typeof $scope.currentIssueLabels === 'string') {
+                    $scope.currentIssueLabels = $scope.currentIssueLabels.split(',');
+                }
+
+                var issueToEdit = {
+                    Title: $scope.currentIssue.Title,
+                    Description: $scope.currentIssue.Description,
+                    DueDate: $scope.currentIssueDueDateLocal,
+                    AssigneeId: $scope.currentIssue.Assignee.Id,
+                    PriorityId: $scope.issuePriority,
+                    Labels: $scope.currentIssueLabels
+                };
+
+                issuesService.editIssue(issueToEdit, $routeParams.id)
+                    .then(function success(data) {
+                        $location.path('issues/' + data.Id);
+                    }, function error(err) {
+                        notificationService.showError('Unable to edit issue', err);
+                    });
+            };
+        }])
 ;
--- app/services/issues.js
+++ app/services/issues.js
@@ -29,11 +29,66 @@ angular.module('issueTracker.services.issues', [])
                 return deferred.promise;
             }
 
-            function addIssue() {
+            function editIssue(issue, id) {
+                var deferred = $q.defer();
 
+                var dataLabels = '';
+                issue.Labels.forEach(function (l, index) {
+                    dataLabels += '&labels[' + index + '].Name=' + l.trim();
+                });
+
+                var data = 'Title=' + issue.Title +
+                    '&Description=' + issue.Description +
+                    '&DueDate=' + issue.DueDate.toISOString() +
+                    '&AssigneeId=' + issue.AssigneeId +
+                    '&PriorityId=' + issue.PriorityId +
+                    dataLabels;
+
+                var req = {
+                    method: 'PUT',
+                    url: BASE_URL + 'issues/' + id,
+                    headers: {
+                        'Authorization': 'Bearer ' + JSON.parse(sessionStorage['currentUser']).access_token,
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    data: data
+                };
+
+                $http(req)
+                    .then(function success(response) {
+                        deferred.resolve(response.data);
+                    }, function error(err) {
+                        console.log(err);
+                        deferred.reject(err);
+                    });
+
+                return deferred.promise;
+            }
+
+            function getIssueById(id) {
+                var deferred = $q.defer();
+
+                var req = {
+                    method: 'GET',
+                    url: BASE_URL + 'issues/' + id,
+                    headers: {
+                        'Authorization': 'Bearer ' + JSON.parse(sessionStorage['currentUser']).access_token,
+                    }
+                };
+
+                $http(req)
+                    .then(function success(response) {
+                        deferred.resolve(response.data);
+                    }, function error(err) {
+                        deferred.reject(err);
+                    });
+
+                return deferred.promise;
             }
 
             return {
-                getUserAssignedIssues: getUserAssignedIssues
+                getUserAssignedIssues: getUserAssignedIssues,
+                editIssue: editIssue,
+                getIssueById: getIssueById
             }
         }]);
